274489659b11398b5b9bd47a41ce577e80bb8783
julialonghi <116280754+julialonghi@users.noreply.github.com>
2026-05-12 23:11:35 +0000

    classe netflix

---
+++ Netflix.java
@@ -0,0 +1,137 @@
+public class Netflix {
+    private String id;
+    private String titulo;
+    private String show_type;
+    private String descricao;
+    private int release_year;
+    private int age_certification;
+    private int runtime;
+    private String generos;
+    private String production_countries;
+    private int temporadas;
+    private String imdb_id;
+    private double imdb_score;
+    private double imdb_votes;
+    private int tmdb_popularity;
+    private int tmdb_score;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getShow_type() {
+        return show_type;
+    }
+
+    public void setShow_type(String show_type) {
+        this.show_type = show_type;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getRelease_year() {
+        return release_year;
+    }
+
+    public void setRelease_year(int release_year) {
+        this.release_year = release_year;
+    }
+
+    public int getAge_certification() {
+        return age_certification;
+    }
+
+    public void setAge_certification(int age_certification) {
+        this.age_certification = age_certification;
+    }
+
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getGeneros() {
+        return generos;
+    }
+
+    public void setGeneros(String generos) {
+        this.generos = generos;
+    }
+
+    public String getProduction_countries() {
+        return production_countries;
+    }
+
+    public void setProduction_countries(String production_countries) {
+        this.production_countries = production_countries;
+    }
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public String getImdb_id() {
+        return imdb_id;
+    }
+
+    public void setImdb_id(String imdb_id) {
+        this.imdb_id = imdb_id;
+    }
+
+    public double getImdb_score() {
+        return imdb_score;
+    }
+
+    public void setImdb_score(double imdb_score) {
+        this.imdb_score = imdb_score;
+    }
+
+    public double getImdb_votes() {
+        return imdb_votes;
+    }
+
+    public void setImdb_votes(double imdb_votes) {
+        this.imdb_votes = imdb_votes;
+    }
+
+    public int getTmdb_popularity() {
+        return tmdb_popularity;
+    }
+
+    public void setTmdb_popularity(int tmdb_popularity) {
+        this.tmdb_popularity = tmdb_popularity;
+    }
+
+    public int getTmdb_score() {
+        return tmdb_score;
+    }
+
+    public void setTmdb_score(int tmdb_score) {
+        this.tmdb_score = tmdb_score;
+    }
+}
